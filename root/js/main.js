8b43d0a61cb1d140f0b5537ad7bb2977ba335a84
manumancione <manuela.mancione@gmail.com>
2022-09-11 11:31:50 -0300

    desafio complementario:arrays

--- root/js/main.js
+++ root/js/main.js
@@ -1,60 +1,148 @@
 // **********************CARRITO DE COMPRAS
 
-const precioAspirina = 100;
-const precioIbuprofeno = 120;
-const precioDiclofenac = 110;
-const precioParacetamol = 90;
-const impuestosIva = 0.21;
+//informacion sobre los medicamentos en venta
 
-let analgesicoElegido = prompt("Si quieres comprar un analgésico de venta libre, podemos ofrecerte los siguientes. selecciona 1, 2, 3 ó 4 según prefieras: \n1-Aspirina Bayer comprimidos 500mg\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg\n3-Diclofenac Actron comprimidos 50mg\n4-Paracetamol Tafirol comprimidos 500mg\n5-Si decides no optar por ninguno.")
+const impuestosIva = 21;
 
+class Medicamento {
+    constructor(nombre, generico, precio) {
+        this.nombreComercial = nombre;
+        this.nombreGenerico = generico;
+        this.precio = precio;
+    }
 
-function elegirAnalgesico(analgesicoElegido) {
-    switch (analgesicoElegido) {
-        case "1":
-            return precioAspirina;
-        case "2":
-            return precioIbuprofeno;
-        case "3":
-            return precioDiclofenac;
-        case "4":
-            return precioParacetamol;
-        default:
-            console.log("solo puedes elegir entre los siguientes valores: \n1-Aspirina Bayer comprimidos 500mg\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg\n3-Diclofenac comprimidos 50mg\n4-Paracetamol comprimidos 500mg\n5-Si decides no optar por ninguno")
-            break; //Lauti: este codigo es inaccesible, tiene sentido que lo escriba?? o por una cuestion de sintaxis, buenas practicas o algo, se recomienda dejar?
+    calculoPrecioConIva() {
+        return (this.precio + this.precio * impuestosIva / 100)
     }
 }
 
-const calculoPrecioConIva = elegirAnalgesico => elegirAnalgesico(analgesicoElegido) + elegirAnalgesico(analgesicoElegido) * impuestosIva / 100;
+const medicamento1 = new Medicamento("Actron", "Diclofenac 50 mg", 110, 1000);
+const medicamento2 = new Medicamento("Ibupirac", "Ibuprofeno 400 mg", 120, 1500);
+const medicamento3 = new Medicamento("Tafirol", "Paracetamol 500 mg", 90, 1000);
+const medicamento4 = new Medicamento("Bayaspirina", "Aspirina 500 mg", 100, 1200);
 
-const informarPrecio = analgesicoElegido => {
-    if (analgesicoElegido == "1" || analgesicoElegido == "2" || analgesicoElegido == "3" || analgesicoElegido == "4") {
-        console.log("El precio final de tu compra, con impuestos incluidos, es de " + calculoPrecioConIva(elegirAnalgesico) + " pesos.")
-    } else if (analgesicoElegido == "5") {
-        console.log("No elegiste ninguna de nuestras opciones de analgésicos.");
-    } else {
-        let asesoramientoFarmaceutico = prompt("No has realizado ninguna compra.Te gustaría que te asesore algún farmacéutico?")
-        if (asesoramientoFarmaceutico == "si" || asesoramientoFarmaceutico == "SI") {
-            console.log("Aguarda unos minutos, te pondremos en contacto con el farmacéutico")
+const medicamentosEnVenta = [medicamento1, medicamento2, medicamento3, medicamento4]
+
+
+
+
+//informacion sobre el cliente/compra
+
+const carroCompras = []
+const comprar = medicamento => {
+    carroCompras.push(medicamento);
+    alert(medicamento.nombreGenerico + " se agregó a tu carro de compras, el precio con impuestos incluidos es de " + medicamento.calculoPrecioConIva() + " pesos.")
+}
+
+
+
+
+
+//compra de medicamentos
+
+alert("Bienvenidos a FarmaWeb.\nPor el momento solo venedomos analgésicos de venta libre.")
+let eleccionCliente = prompt("Elige una de las siguientes opciones.\n1- Comprar analgésicos.\n2- LLamar un Farmacéutico para mayor asesoramiento.\n3- Salir.")
+
+while (eleccionCliente != "1" && eleccionCliente != "2" && eleccionCliente != "3") {
+    eleccionCliente = prompt("Elige una de las siguientes opciones.\n1- Comprar analgésicos.\n2- LLamar un Farmacéutico para mayor asesoramiento.\n3- Salir.")
+}
+
+switch (eleccionCliente) {
+    case "1":
+        let analgesicoElegido = prompt("Si quieres comprar un analgésico de venta libre, podemos ofrecerte los siguientes. Selecciona 1, 2, 3 ó 4 según prefieras: \n1-Aspirina Bayer comprimidos 500mg\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg\n3-Diclofenac Actron comprimidos 50mg\n4-Paracetamol Tafirol comprimidos 500mg\n5-Si decides no optar por ninguno.")
+
+        while (analgesicoElegido != "1" && analgesicoElegido != "2" && analgesicoElegido != "3" && analgesicoElegido != "4" && analgesicoElegido != "5") {
+            analgesicoElegido = prompt("Si quieres comprar un analgésico de venta libre, podemos ofrecerte los siguientes. selecciona 1, 2, 3 ó 4 según prefieras: \n1-Aspirina Bayer comprimidos 500mg\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg\n3-Diclofenac Actron comprimidos 50mg\n4-Paracetamol Tafirol comprimidos 500mg\n5-Si decides no optar por ninguno.")
         }
-    }
+
+        switch (analgesicoElegido) {
+            case "1":
+                comprar(medicamento4);                
+                break;
+            case "2":
+                comprar(medicamento2);                
+                break;
+            case "3":
+                comprar(medicamento1);
+                break;
+            case "4":
+                comprar(medicamento3);                
+                break;
+            case "5":
+                alert("No elegiste ninguna de nuestras opciones de analgésicos. Te esperamos para una nueva compra. Gracias.");
+                break;
+        }
+
+
+        let nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Si quieres finalizar tu compra.")
+
+        while (nuevaCompra == "1") {
+
+            analgesicoElegido = prompt("Si quieres comprar un analgésico de venta libre, podemos ofrecerte los siguientes. Selecciona 1, 2, 3 ó 4 según prefieras: \n1-Aspirina Bayer comprimidos 500mg.\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg.\n3-Diclofenac Actron comprimidos 50mg.\n4-Paracetamol Tafirol comprimidos 500mg.\n5-Si decides no optar por ninguno.")
+
+            switch (analgesicoElegido) {
+                case "1":
+                    comprar(medicamento4);                   
+                    nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Si quieres finalizar tu compra.")
+                    break;
+                case "2":
+                    comprar(medicamento2);                    
+                    nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Si quieres finalizar tu compra.")
+                    break;
+                case "3":
+                    comprar(medicamento1);                    
+                    nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Si quieres finalizar tu compra.")
+                    break;
+                case "4":
+                    comprar(medicamento3);                    
+                    nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Si quieres finalizar tu compra.")
+                    break;
+                case "5":
+                    alert("No elegiste ninguna de nuestras opciones de analgésicos. Te esperamos para una nueva compra. Gracias.");
+                    break;
+            }
+        }
+
+        let totalCompra = 0
+        for (total of carroCompras) {
+            totalCompra += total.calculoPrecioConIva()
+        }
+
+        if (nuevaCompra == "2") {
+            alert("Gracias por tu compra, el precio total que debes abonar es de " + totalCompra + " pesos.");
+            console.log(carroCompras)
+        }
+        break;
+
+    case "2":
+        alert("Aguarda unos minutos, te pondremos en contacto con el Farmacéutico.");
+        break;
+    case "3":
+        alert("Saliste del sistema de compras. Esperamos tu nueva visita. Gracias.");
+        break;
+    default:
+        alert("Lo siento, no es una opcion válida.");
+        break;
 }
 
-informarPrecio(analgesicoElegido);
-
-// **********************GESTION DE RESERVA DE TURNOS
 
 
-// let turno = prompt("Seleccioná:\n1- Si querés un turno para que se te mida la presión, o\n2- Si querés un turno para hacerte análisis de laboratorio.")
 
-// const pedirTurno = turno => {
-//     while (turno != "1" && turno != "2") {
-//         turno = prompt("Selecciona:\n 1- Si querés un turno para que se te mida la presion, o\n 2- Si querés un turno para hacerte análisis de laboratorio.")
-//     }
-//     if (turno == "1" || turno == "2") {
-//         alert("Seleccioná el día y el horario que más te convenga.")
-//     }
-// }
 
-// pedirTurno(turno);
+
+// // **********************GESTION DE RESERVA DE TURNOS
+
+
+// // let turno = prompt("Seleccioná:\n1- Si querés un turno para que se te mida la presión, o\n2- Si querés un turno para hacerte análisis de laboratorio.")
+
+// // const pedirTurno = turno => {
+// //     while (turno != "1" && turno != "2") {
+// //         turno = prompt("Selecciona:\n 1- Si querés un turno para que se te mida la presion, o\n 2- Si querés un turno para hacerte análisis de laboratorio.")
+// //     }
+// //     if (turno == "1" || turno == "2") {
+// //         alert("Seleccioná el día y el horario que más te convenga.")
+// //     }
+// // }
+
+// // pedirTurno(turno);
 
