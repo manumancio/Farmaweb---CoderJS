fee3797da7e41f8dde4ff458836595fc9a21747a
manumancione <manuela.mancione@gmail.com>
2022-09-19 09:49:54 -0300

    entrega desafio interactuar con HTML

--- root/js/main.js
+++ root/js/main.js
@@ -1,201 +1,89 @@
-// **********************CARRITO DE COMPRAS
-
-//informacion sobre los medicamentos en venta
-const impuestosIva = 21;
-
-class Medicamento {
-    constructor(nombre, generico, precio) {
-        this.nombreComercial = nombre;
-        this.nombreGenerico = generico;
-        this.precio = precio;
-    }
-
-    calculoPrecioConIva() {
-        return (this.precio + this.precio * impuestosIva / 100)
-    }
-}
-
-const medicamento1 = new Medicamento("Actron", "Diclofenac 50 mg", 110, 1000);
-const medicamento2 = new Medicamento("Ibupirac", "Ibuprofeno 400 mg", 120, 1500);
-const medicamento3 = new Medicamento("Tafirol", "Paracetamol 500 mg", 90, 1000);
-const medicamento4 = new Medicamento("Bayaspirina", "Aspirina 500 mg", 100, 1200);
-
-const medicamentosEnVenta = [];
-
-medicamentosEnVenta.push(medicamento1);
-medicamentosEnVenta.push(medicamento2);
-medicamentosEnVenta.push(medicamento3);
-medicamentosEnVenta.push(medicamento4);
-
-
 //array carro de compras inicial
 const carroCompras = []
 
 
-//funcion que muestra menu inicial
-function menuInicial() {
-    alert("Bienvenidos a FarmaWeb.\nPor el momento solo vendemos analgésicos de venta libre.")
-    let eleccionCliente = prompt("Elige una de las siguientes opciones.\n1- Comprar analgésicos.\n2- LLamar un Farmacéutico para mayor asesoramiento.\n3- Salir.")
+// funcion para mostrar productos
+const productsContainer = document.getElementById("productsContainer");
 
-    while (eleccionCliente != "1" && eleccionCliente != "2" && eleccionCliente != "3") {
-        eleccionCliente = prompt("Elige una de las siguientes opciones.\n1- Comprar analgésicos.\n2- LLamar un Farmacéutico para mayor asesoramiento.\n3- Salir.")
-    }
-    return eleccionCliente
+function mostrarProductos(items) {
+    items.forEach(element => {
+        let card = document.createElement("div")
+        card.classList.add("card")
+        card.style = "width: 18rem;"
+        card.innerHTML = `
+            <img src="${element.imagen}" class="card-img-top" alt="Imagen ilustrativa del medicamento ${element.nombreComercial}">
+            <div class="card-body">
+            <h5 class="card-title">${element.nombreComercial}</h5>
+            <p class="card-text">${element.nombreGenerico}</p>
+            <p class="card-text">$ ${element.precio}</p>
+            <button id="agregar${element.nombreComercial}" class="btn btn-success ">Agregar <i class="bi bi-bag-plus"></i></button>
+            <div class="d-flex flex-start mt-2 ">
+
+            <button id="eliminar${element.nombreComercial}" class="btn btn-outline-success" type="button"> - </button>
+
+            <p id="valorValue" type="text" class="input-carroDeCompras">1</p>
+
+            <button  class="btn btn-outline-success" type="button"> + </button>
+
+            </div>
+            </div> 
+        `
+        productsContainer.appendChild(card)
+
+        // boton "Agregar" que agrega productos
+        const botonAgregar = document.getElementById(`agregar${element.nombreComercial}`)
+        botonAgregar.addEventListener("click", () => {
+            console.log(`${element.nombreComercial}`)
+            carroCompras.push(element)
+            alert(`Agregaste a tu carro de compras: ${element.nombreComercial}`)
+            console.log(carroCompras)
+            document.getElementById("valorValue")
+            valorValue.innerHTML = `<p> (+=1)</p>`
+        })
+
+        // boton "-" que elimina productos
+        const botonEliminar = document.getElementById(`eliminar${element.nombreComercial}`)
+        botonEliminar.addEventListener("click", () => {
+            const medicamentoABorrar = carroCompras.find(element => element.nombreComercial)
+            const indice = carroCompras.indexOf(medicamentoABorrar);
+            carroCompras.splice(indice, 1);
+            console.log(carroCompras)
+        })
+
+         // boton "+" que agrega productos
+
+    });
 }
 
-
-//funcion consulta sobre las opciones de analgesicos disponibles
-function opcionesAnalgesico() {
-    let analgesicoElegido = prompt("Si quieres comprar un analgésico de venta libre, podemos ofrecerte los siguientes. Selecciona 1, 2, 3 ó 4 según prefieras: \n1-Aspirina Bayer comprimidos 500mg\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg\n3-Diclofenac Actron comprimidos 50mg\n4-Paracetamol Tafirol comprimidos 500mg\n5-Si decides no optar por ninguno.")
-
-    while (analgesicoElegido != "1" && analgesicoElegido != "2" && analgesicoElegido != "3" && analgesicoElegido != "4" && analgesicoElegido != "5") {
-        analgesicoElegido = prompt("Si quieres comprar un analgésico de venta libre, podemos ofrecerte los siguientes. selecciona 1, 2, 3 ó 4 según prefieras: \n1-Aspirina Bayer comprimidos 500mg\n2-Ibuprofeno Ibupirac cápsulas blandas 400mg\n3-Diclofenac Actron comprimidos 50mg\n4-Paracetamol Tafirol comprimidos 500mg\n5-Si decides no optar por ninguno.")
-    }
-    return analgesicoElegido
-}
+mostrarProductos(medicamentosEnVenta)
 
 
-//funcion para sumar un medicamento al carro de compras
-function sumarMedicamento(medicamento) {
-    carroCompras.push(medicamento);
-    alert(medicamento.nombreGenerico + " se agregó a tu carro de compras, el precio con impuestos incluidos es de " + medicamento.calculoPrecioConIva() + " pesos.");
-    let nuevaCompra = seguirComprando();
-    segundoMenu(nuevaCompra)
-}
 
-
-//funcion consulta sobre nueva compra
-function seguirComprando() {
-    let nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Mostrar tu carrito de compras.\n3- Eliminar algun item del carrito de compras.\n4- Si quieres finalizar tu compra.");
-
-    while (nuevaCompra != "1" && nuevaCompra != "2" && nuevaCompra != "3" && nuevaCompra != "4") {
-        nuevaCompra = prompt("Si quieres presiona:\n1- Para agregar otro analgésico.\n2- Mostrar tu carrito de compras.\n3- Eliminar algun item del carrito de compras.\n4- Si quieres finalizar tu compra.");
-    }
-    return nuevaCompra;
-}
-
-
-//funcion para mostrar carrito e compras
-function mostrarCarrito() {
-    let carrito = "aún está vacío"
-    carrito = carroCompras.map(medicamento => medicamento.nombreGenerico);
-    alert("Ítems de tu carro de compra: " + carrito);
-    let nuevaCompra = seguirComprando();
-    segundoMenu(nuevaCompra)
-}
-
-
-//funcion para eliminar un medicamento del carro
-function eliminarMedicamento() {
-    let carrito = "aún está vacío"
-    carrito = carroCompras.map(medicamento => medicamento.nombreComercial);
-    let medicamentoABorrar = prompt(`Por el momento tienes en tu carro de compras ${carrito}. Indica el nombre comercial del medicamento que deseas eliminar de tu carro de compras (Bayaspirina, Ibupirac, Actron, Tafirol).`);
-    let medicamento = carroCompras.find(medicamento => medicamento.nombreComercial === medicamentoABorrar)
-    let indice = carroCompras.indexOf(medicamento);
-    carroCompras.splice(indice, 1);
-    console.log(carroCompras);
-    let nuevaCompra = seguirComprando();
-    segundoMenu(nuevaCompra);
-}
-
+//boton finalizar compra
 
 //funcion del cálculo del precio total de la compra
-function calculoTotal() {
+function calcularTotal() {
     let totalCompra = 0;
     for (total of carroCompras) {
-        totalCompra += total.calculoPrecioConIva()
+        totalCompra += total.calcularPrecioConIva()
     }
-    alert("Gracias por tu compra, el precio total que debes abonar es de " + totalCompra.toFixed(2) + " pesos.");
     console.log(carroCompras);
+    return totalCompra.toFixed(2);
 }
 
 
-//funcion compra analgesico del menu
-function eleccionMedicamento() {
-    let analgesicoElegido = opcionesAnalgesico();
-    switch (analgesicoElegido) {
-        case "1":
-            sumarMedicamento(medicamento4);
-            break;
-        case "2":
-            sumarMedicamento(medicamento2);
-            break;
-        case "3":
-            sumarMedicamento(medicamento1);
-            break;
-        case "4":
-            sumarMedicamento(medicamento3);
-            break;
-        case "5":
-            alert("No elegiste ninguna de nuestras opciones de analgésicos.");
-            calculoTotal()
-            break;
-    }
+//funcion para mostrar carrito de compras
+function mostrarCarrito() {
+    let carrito = "aún está vacío"
+    carrito = carroCompras.map(element => element.nombreComercial);
+    return carrito;
 }
 
 
-//funcion eleccion del segundo menu
-const segundoMenu = nuevaCompra => {
-    //comprar otro analgesico
-    if (nuevaCompra == "1") {
-        eleccionMedicamento()
-
-        //mostrar el carro de compras
-    } else if (nuevaCompra == "2") {
-        mostrarCarrito()
-
-        //eliminar item del carro de compras
-    } else if (nuevaCompra == "3") {
-        eliminarMedicamento()
-
-        //finalizacion de la compra
-    } else if (nuevaCompra == "4") {
-        calculoTotal()
-
-    } else {
-        alert("La opción no es válida.")
-    }
-}
-
-
-
-
-//EJECUCION DEL PROGRAMA
-
-let eleccionCliente = menuInicial()
-
-switch (eleccionCliente) {
-    case "1":
-        eleccionMedicamento()
-        break;
-    case "2":
-        alert("Aguarda unos minutos, te pondremos en contacto con el Farmacéutico.");
-        break;
-    case "3":
-        alert("Saliste del sistema de compras. Esperamos tu nueva visita. Gracias.");
-        break;
-    default:
-        alert("Lo siento, no es una opcion válida.");
-        break;
-}
-
-
-
-
-
-
-// // **********************GESTION DE RESERVA DE TURNOS
-
-
-// // let turno = prompt("Seleccioná:\n1- Si querés un turno para que se te mida la presión, o\n2- Si querés un turno para hacerte análisis de laboratorio.")
-
-// // const pedirTurno = turno => {
-// //     while (turno != "1" && turno != "2") {
-// //         turno = prompt("Selecciona:\n 1- Si querés un turno para que se te mida la presion, o\n 2- Si querés un turno para hacerte análisis de laboratorio.")
-// //     }
-// //     if (turno == "1" || turno == "2") {
-// //         alert("Seleccioná el día y el horario que más te convenga.")
-// //     }
-// // }
-
-// // pedirTurno(turno);
+const botonFinalizar = document.getElementById("botonFinalizarCompra")
+botonFinalizarCompra.addEventListener("click", () => {
+    document.getElementById("resultadosCompra")
+    resultadosCompra.innerHTML = `<h3>Mi carro de compras</h3>
+                                  <p>Ítems de tu carro de compra: ${mostrarCarrito()}</p>
+                                  <p>El precio final que debes abonar es es $ ${calcularTotal()}.</p>`
+})
---
+++ root/js/products.js
@@ -0,0 +1,46 @@
+//informacion sobre los medicamentos en venta
+
+const impuestosIva = 21;
+
+class Medicamento {
+    constructor(nombre, generico, precio, stock, imagen) {
+        this.nombreComercial = nombre;
+        this.nombreGenerico = generico;
+        this.precio = precio;
+        this.stock = stock;
+        this.imagen = imagen;
+    }
+
+    calcularPrecioConIva() {
+        return (this.precio + this.precio * impuestosIva / 100)
+    }
+}
+
+const actron = new Medicamento("Actron", "Ibuprofeno 400 mg", 110, 1000, "./images/rsz_actron.png");
+const actronMujer = new Medicamento("Actron", "Ibuprofeno 400 mg", 110, 1000, "./images/rsz_actronMujer.jpg");
+const actronPediatrico = new Medicamento("Actron Pediátrico", "Ibuprofeno 2%", 110, 1000, "./images/rsz_actronPediatrico.png");
+const ibupirac = new Medicamento("Ibupirac", "Ibuprofeno 400 mg", 120, 1500, "./images/rsz_ibupirac.png");
+const ibuevanolForte = new Medicamento("Ibuevanol Forte", "Ibuprofeno 400 mg", 125, 500, "./images/rsz_ibuevanolForte.png");
+const bayaspirina = new Medicamento("Bayaspirina", "Aspirina 500 mg", 100, 1200, "./images/rsz_bayaspirina.jpg");
+const aspirinetas = new Medicamento("Aspirinetas", "Aspirina 100 mg", 105, 500, "./images/rsz_aspirineta.jpg");
+const novalgina = new Medicamento("Novalgina", "Dipirona 500 mg", 115, 500, "./images/rsz_novalgina.jpg");
+const naproxeno = new Medicamento("Naproxeno Laproff", "Naproxeno 500 mg", 135, 500, "./images/rsz_naproxeno.png");
+const tafirol = new Medicamento("Tafirol", "Paracetamol 500 mg", 90, 1000, "./images/rsz_tafirol.jpg");
+const tafirolPediatrico = new Medicamento("Tafirol Pediátrico", "Paracetamol 120mg/5ml", 145, 500, "./images/rsz_tafirolPediatrico.jpg");
+const geniol = new Medicamento("Geniol", "Paracetamol 500 mg", 90, 500, "./images/rsz_geniol.png");
+
+
+const medicamentosEnVenta = [];
+
+medicamentosEnVenta.push(actron);
+medicamentosEnVenta.push(actronMujer);
+medicamentosEnVenta.push(actronPediatrico);
+medicamentosEnVenta.push(ibupirac);
+medicamentosEnVenta.push(ibuevanolForte);
+medicamentosEnVenta.push(bayaspirina);
+medicamentosEnVenta.push(aspirinetas);
+medicamentosEnVenta.push(novalgina);
+medicamentosEnVenta.push(naproxeno);
+medicamentosEnVenta.push(tafirol);
+medicamentosEnVenta.push(tafirolPediatrico);
+medicamentosEnVenta.push(geniol);
